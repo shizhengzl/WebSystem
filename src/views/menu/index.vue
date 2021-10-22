d0242956b5705edf8afe15a130c2fa2e7f41627c
shizheng <shizheng@jizhibao.com.cn>
2021-10-22 18:07:20 +0800

    1 菜单维护功能

--- src/views/menu/index.vue
+++ src/views/menu/index.vue
@@ -2,116 +2,104 @@
 <template>
   <div class="app-container">
     <div class="dialog-toolbar">
-      <LinkButton iconCls="icon-add" :plain="true" @click="$refs.dialogs.open()">添加</LinkButton>
-      <LinkButton iconCls="icon-edit" :plain="true">修改</LinkButton>
+      <LinkButton iconCls="icon-add" :plain="true" @click="create()">添加</LinkButton>
+      <LinkButton iconCls="icon-edit" :plain="true" @click="modify()">修改</LinkButton>
       <LinkButton iconCls="icon-remove" :plain="true">删除</LinkButton>
       <SearchBox style="width:300px"
                  placeholder="搜索"
-                 v-model="value"
+                 v-model="searchvalue"
                  @search="onSearch($event)">
-
       </SearchBox>
-
       <LinkButton iconCls="icon-help" :plain="true">Help</LinkButton>
     </div>
 
-
-
-    <TreeGrid :data="data" style="height:250px" idField="id" treeField="menuName">
-
+    <TreeGrid :data="data" style="height:250px" idField="id"
+              checkbox
+              cascadeCheck ="false"
+              treeField="menuName" ref="grids">
       <template v-for="(item,index) in header">
         <GridColumn :field="item.columnName"
                     :title="item.columnDescription || item.columnName"></GridColumn>
       </template>
-
-      <!--<GridColumn field="itemid" title="Item ID"></GridColumn>
-    <GridColumn field="name" title="Name"></GridColumn>
-    <GridColumn field="listprice" title="List Price" align="right"></GridColumn>
-    <GridColumn field="unitcost" title="Unit Cost" align="right"></GridColumn>
-    <GridColumn field="attr" title="Attribute" width="30%"></GridColumn>
-    <GridColumn field="status" title="Status" align="center"></GridColumn>-->
     </TreeGrid>
 
-
     <Dialog :title="'添加菜单'" ref="dialogs"
-            :dialogStyle="{width:'600px',height:'500px'}"
+            :dialogStyle="{width:'550px',height:'350px'}"
             :modal="true"
-             closed="true"
+             closed
             :draggable="true"
-            :resizable="true">
-
+            :resizable="true"> 
       <Form ref="form" :model="menus" :rules="rules" @validate="errors=$event">
         <div style="margin:20px">
-          <TextBox inputId="menuName" name="menuName" v-model="menus.menuName" v-Tooltip="getTipOpts('menuName')">
+          <TextBox inputId="menuName" name="menuName" v-model="menus.menuName" v-Tooltip="getTipOpts('menuName')" style="width:200px">
             <Addon align="left">
               <LinkButton :btnStyle="{borderRadius:0,borderWidth:'0 1px 0 0',width:'70px',height:'30px'}">菜单名称</LinkButton>
             </Addon>
           </TextBox>
+          <span style="margin-left:30px;"></span>
 
-          <TextBox inputId="menuId" name="menuId" v-model="menus.menuId" v-Tooltip="getTipOpts('menuId')">
-            <Addon align="left">
-              <LinkButton :btnStyle="{borderRadius:0,borderWidth:'0 1px 0 0',width:'70px',height:'30px'}">父级菜单</LinkButton>
-            </Addon>
-          </TextBox>
+          <ComboTree style="width:200px"
+                     v-model="menus.menuId"
+                     :data="data"
+                     :multiple="false"
+                     valueField="id"
+                     textField="menuName" >
+            <Tree slot="tree">
+              <template slot-scope="{node}">
+                <span> {{node.menuName}}</span> 
+              </template>
+
+            </Tree>
+          </ComboTree>
+          <!--<TextBox inputId="menuId" name="menuId" v-model="menus.menuId" v-Tooltip="getTipOpts('menuId')" style="width:200px">
+    <Addon align="left">
+      <LinkButton :btnStyle="{borderRadius:0,borderWidth:'0 1px 0 0',width:'70px',height:'30px'}">父级菜单</LinkButton>
+    </Addon>
+  </TextBox>-->
         </div>
-
         <div style="margin:20px">
-          <TextBox inputId="path" name="path" v-model="menus.path" v-Tooltip="getTipOpts('path')">
+          <TextBox inputId="path" name="path" v-model="menus.path"    style="width:200px">
             <Addon align="left">
               <LinkButton :btnStyle="{borderRadius:0,borderWidth:'0 1px 0 0',width:'70px',height:'30px'}">菜单路劲</LinkButton>
             </Addon>
           </TextBox>
-
-          <TextBox inputId="component" name="component" v-model="menus.component" v-Tooltip="getTipOpts('component')">
+          <span style="margin-left:30px;"></span>
+          <TextBox inputId="component" name="component" v-model="menus.component"   style="width:200px">
             <Addon align="left">
               <LinkButton :btnStyle="{borderRadius:0,borderWidth:'0 1px 0 0',width:'70px',height:'30px'}">菜单组件</LinkButton>
             </Addon>
           </TextBox>
         </div>
-
         <div style="margin:20px">
-          <TextBox inputId="menuIcon" name="menuIcon" v-model="menus.menuIcon" v-Tooltip="getTipOpts('menuIcon')">
+          <TextBox inputId="menuIcon" name="menuIcon" v-model="menus.menuIcon"  style="width:200px">
             <Addon align="left">
               <LinkButton :btnStyle="{borderRadius:0,borderWidth:'0 1px 0 0',width:'70px',height:'30px'}">菜单图标</LinkButton>
             </Addon>
           </TextBox>
-
-          <TextBox inputId="sort" name="sort" v-model="menus.sort" v-Tooltip="getTipOpts('sort')">
+          <span style="margin-left:30px;"></span>
+          <TextBox inputId="sort" name="sort" v-model="menus.sort" style="width:200px">
             <Addon align="left">
               <LinkButton :btnStyle="{borderRadius:0,borderWidth:'0 1px 0 0',width:'70px',height:'30px'}">菜单排序</LinkButton>
             </Addon>
           </TextBox>
         </div>
-
-
         <div style="margin:20px">
-          <CheckBox inputId="isDefault" name="isDefault" v-model="menus.isDefault">
-            <Addon align="left">
-              <LinkButton :btnStyle="{borderRadius:0,borderWidth:'0 1px 0 0',width:'70px',height:'30px'}">默认菜单</LinkButton>
-            </Addon>
-          </CheckBox>
-
-          <CheckBox inputId="isSupper" name="isSupper" v-model="menus.isSupper">
-            <Addon align="left">
-              <LinkButton :btnStyle="{borderRadius:0,borderWidth:'0 1px 0 0',width:'70px',height:'30px'}">超级管理员菜单</LinkButton>
-            </Addon>
-          </CheckBox>
+          <SwitchButton inputId="isDefault" v-model="menus.isDefault" onText="默认菜单" offText="非默认菜单" style="width:200px;">
+          </SwitchButton>
+          <span style="margin-left:30px;"></span>
+          <SwitchButton inputId="isSupper" name="isSupper" v-model="menus.isSupper" onText="超级管理员菜单" offText="非超级管理员菜单" style="width:200px;">
+          </SwitchButton>
         </div>
-
       </Form>
-
       <template slot="footer">
         <div class="dialog-button">
           <LinkButton style="width:80px" iconCls="icon-cancel" @click="$refs.dialogs.close()">Close</LinkButton>
           <LinkButton style="width:80px" iconCls="icon-save" @click="">Save</LinkButton>
         </div>
       </template>
-
     </Dialog>
-
   </div>
 </template>
- 
 <script>
   import { GetListHeader, GetSupperMenus } from '@/api/menus'
   export default {
@@ -122,6 +110,13 @@
     },
     data() {
       return {
+        searchvalue: "",
+        rules: {
+          menuName: {
+            required: true,
+            length:[5,10] 
+          }
+        },
         errors: {},
         menus: {},
         header:[],
@@ -130,10 +125,19 @@
     },
     // 方法
     methods: {
+      create: function () {
+        this.$refs.dialogs.open();
+      },
+      modify: function () {
+        this.$refs.dialogs.open();
+        debugger
+        var selected = this.$refs.grids.getSelectedRows('checked');
+        debugger
+      },
       getTipOpts(name) {
         return {
           content: this.getError(name),
-          closed: !this.hasError(name),
+          closed: !!!this.hasError(name),
           position: "right"
         };
       },
@@ -142,8 +146,8 @@
           ? this.errors[name][0]
           : null;
       },
-      hasError(name) {
-        return this.getError(name) != null;
+      hasError(name) { 
+        return !!this.getError(name);
       },
       getHeader: function () {
         const owner = this
